69ecec00eaa9081818e7732a5adc076b0de1095d
Thomas Yap <yaptathong@gmail.com>
2024-03-30 07:01:46 +0800

    Comments added

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -9,9 +9,13 @@ public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
 		//var game = new MarioGame();
-		var game = new PacmanGame();
 		//var game = new SuperContraGame();
-		var gameRunner = new GameRunner(game);
+		
+		var game = new PacmanGame();			
+		//object creation
+		var gameRunner = new GameRunner(game);	
+		//object creation + Wiring of dependencies
+		//game is a Dependency of GameRunner 
 		gameRunner.run();
 		
 	}
