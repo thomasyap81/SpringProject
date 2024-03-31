268cfc989ad98e7190b88084933df4ff5ad0d6c2
Thomas Yap <yaptathong@gmail.com>
2024-03-31 21:43:16 +0800

    More Beans in Configurations with record

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,11 +3,34 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//Eliminate verbosity in creating Java Beans
+//Public accessor methods, constructor,equals,hashcode 
+//and toString are automatically are auto created
+//Released in JDK 16.
+
+record Person (String name, int age) {};
+record Address (String firstLine, String city) {};
+
 @Configuration
 public class HelloWorldConfiguration {
 	
 	@Bean
 	public String name() {
 		return "Thomas";
+	}
+	
+	@Bean
+	public int age() {
+		return 15;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Thomas",20);
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("Serangoon","Singapore");
 	}
 }
