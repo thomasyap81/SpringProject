30e99d57dd7c259fcc5bbd5e3b05e5be8ac93869
Thomas Yap <yaptathong@gmail.com>
2024-03-31 22:24:30 +0800

    Implementing Auto Wiring in Spring Framework Java configuration File

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 //and toString are automatically are auto created
 //Released in JDK 16.
 
-record Person (String name, int age) {};
+record Person (String name, int age,Address address) {};
 record Address (String firstLine, String city) {};
 
 @Configuration
@@ -26,11 +26,26 @@ public class HelloWorldConfiguration {
 	
 	@Bean
 	public Person person() {
-		return new Person("Thomas",20);
+		return new Person("Thomas",20, new Address("Macpherson","Singapore"));
 	}
 	
 	@Bean
+	public Person person2MethodCall() {
+		return new Person(name(),age(),address());
+	}
+	
+	@Bean
+	public Person person3Parameters(String name, int age, Address address3) {
+		return new Person(name,age,address3); //using names of parameters as arguments from other beans
+	}
+	
+	@Bean (name ="address2")
 	public Address address() {
 		return new Address("Serangoon","Singapore");
+	}
+	
+	@Bean (name ="address3")
+	public Address address3() {
+		return new Address("Menglembu","Ipoh");
 	}
 }
