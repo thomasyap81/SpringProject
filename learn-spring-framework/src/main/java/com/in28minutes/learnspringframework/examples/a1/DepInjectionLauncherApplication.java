74173f1d7b67c818d7c6a3002df8f295a05e2472
Thomas Yap <yaptathong@gmail.com>
2024-04-04 01:07:27 +0800

    Types of Dependency Injection Examples

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
@@ -0,0 +1,21 @@
+package com.in28minutes.learnspringframework.examples.a1;
+
+
+import java.util.Arrays;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan ("com.in28minutes.learnspringframework.examples.a1")
+public class DepInjectionLauncherApplication {
+	
+	public static void main(String[] args) {
+		try(var context = 
+				new AnnotationConfigApplicationContext
+					(DepInjectionLauncherApplication.class)){
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+		}
+	}
+}
+
